e1293aa4fa5e10061546e627d4c8ce3daacd0afb
ppigs <99869414+ppigs@users.noreply.github.com>
2022-02-21 16:45:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # 添加 feed 源
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # adguardhome插件
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
